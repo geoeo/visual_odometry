32797db9fddb59a4bc8ae148b33088bfca9a82c7
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2019-05-02 23:30:52 +0200

    example formatting 2

--- examples/image_gradient.rs
+++ examples/image_gradient.rs
@@ -9,11 +9,15 @@ use visual_odometry::image::filters::types::ImageFilter;
 
 fn main() {
 
-    let image_path = "images/ferris.png";
-    let file_out_path = "output/gradient_test.png";
+    let image_name = "ferris";
+    let image_format = "png";
+    let image_filter = ImageFilter::ScharrX;
+
+    let image_path = format!("images/{}.{}",image_name, image_format);
+    let file_out_path = format!("output/{}_{:?}.{}",image_name,image_filter,image_format);
 
     let image = image::open(&Path::new(&image_path)).unwrap().grayscale().to_luma();
-    let kernel = select_filter(ImageFilter::ScharrX);
+    let kernel = select_filter(image_filter);
 
     let gradient_image = filter3x3(&image,&kernel);
 
